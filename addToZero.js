6b0f68f572a7ddf266d89a01950ccabbf39bc8f3
Diana Lupita Garcia <75147104+DianaLuGarcia@users.noreply.github.com>
2022-04-24 21:18:20 -0600

    done

---
+++ addToZero.js
@@ -0,0 +1,13 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+function addToZero(array) {
+for (i = 0; i < array.length; i++){
+    if(array[i] + array[i] == 0){
+    console.log(true);
+        }else {
+        console.log(false);
+        }
+    }       
+}
+console.log(addToZero(array));
+/* Write your solution below: */
